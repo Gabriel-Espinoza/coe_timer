f926ce1da2cd0b36d3319a4a9d4e1f85e649b32b
Gabriel Espinoza <gab.espinoza@gmail.com>
2020-12-10 23:44:31 -0300

    Primera versión medianamente funcional de las clases

--- lib/classSelector.dart
+++ lib/classSelector.dart
@@ -1,38 +1,39 @@
+import 'package:coe_timer/enums.dart';
 import 'package:flutter/material.dart';
 
 //Text classSelector = new Text('Class Selector goes here');
 
-// List<String> classes = [
-//   'assets/barbarian_male.png',
-//   'assets/crusader_male.png',
-//   'assets/demonhunter_male.png',
-//   'assets/monk_male.png',
-//   'assets/necromancer_male.png',
-//   'assets/witchdoctor_male.png',
-//   'assets/wizard_male.png'
-// ];
+List<String> classes = [
+  'assets/barbarian_male.png',
+  'assets/crusader_male.png',
+  'assets/demonhunter_male.png',
+  'assets/monk_male.png',
+  'assets/necromancer_male.png',
+  'assets/witchdoctor_male.png',
+  'assets/wizard_male.png'
+];
 
-// GridView classSelector = GridView.count(
-//   // Create a grid with 2 columns. If you change the scrollDirection to
-//   // horizontal, this produces 2 rows.
-//   crossAxisCount: 4,
-//   // Generate 100 widgets that display their index in the List.
-//   children: List.generate(7, (index) {
-//     return Center(
-//       //     child: IconButton(
-//       //   icon: Image.asset(classes[index]),
-//       //   iconSize: 50,
-//       //   onPressed: () {},
-//       // )
-//       child: Text(
-//         'Item $index',
-//       ),
-//     );
-//   }),
-// );
-
-IconButton classSelector = IconButton(
-  icon: Image.asset('assets/barbarian_female.png'),
-  iconSize: 50,
-  onPressed: () {},
+Expanded classSelector = new Expanded(
+  child: GridView.count(
+    // Create a grid with 2 columns. If you change the scrollDirection to
+    // horizontal, this produces 2 rows.
+    crossAxisCount: 4,
+    // Generate 100 widgets that display their index in the List.
+    children: List.generate(HeroClass.values.length, (index) {
+      return Center(
+          child: IconButton(
+        icon: Image.asset(classes[index]),
+        iconSize: 50,
+        onPressed: () {
+          print('Selected Hero = ${HeroClass.values[index]}');
+        },
+      ));
+    }),
+  ),
 );
+
+// IconButton classSelector = IconButton(
+//   icon: Image.asset('assets/barbarian_female.png'),
+//   iconSize: 50,
+//   onPressed: () {},
+// );
---
+++ lib/enums.dart
@@ -0,0 +1,11 @@
+enum HeroClass {
+  barbarian,
+  crusader,
+  demonhunter,
+  monk,
+  necromancer,
+  witchdoctor,
+  wizard_male,
+}
+
+enum Gender { female, male }
