34b211a148165878b46bb8dd593f806e9b4a20d8
Gabriel Espinoza <gab.espinoza@gmail.com>
2020-12-10 22:41:57 -0300

    PoC usando IconButton

--- lib/classSelector.dart
+++ lib/classSelector.dart
@@ -2,19 +2,8 @@ import 'package:flutter/material.dart';
 
 //Text classSelector = new Text('Class Selector goes here');
 
-ClipOval classSelector = ClipOval(
-  child: Material(
-    color: Colors.blue, // button color
-    child: InkWell(
-      splashColor: Colors.red, // inkwell color
-      child: SizedBox(
-          width: 56,
-          height: 56,
-          child: FlatButton(
-              onPressed: null,
-              padding: EdgeInsets.all(0.0),
-              child: Image.asset('path/the_image.png'))),
-      onTap: () {},
-    ),
-  ),
+IconButton classSelector = IconButton(
+  icon: Image.asset('assets/Arcano.jpg'),
+  iconSize: 50,
+  onPressed: () {},
 );
